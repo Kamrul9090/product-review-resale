11888445904420cb878847867421268c460adb05
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2022-11-23 22:18:55 +0600

    initialization done

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('server running')
+});
+
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+})
+
