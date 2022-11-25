e1fc5eb349a01314d04cca6908e7ed88921f610d
Kamrul Hasan <CR7.kamrul.9090@gmail.com>
2022-11-25 18:37:58 +0600

    added mongodb client

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors())
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER_}:${process.env.DB_PASSWORD_}@cluster0.zvviljv.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const categoriesCollections = client.db('WatchesData').collection('watchCategories');
+
+        app.get('/categories', async (req, res) => {
+            const query = {};
+            const result = await categoriesCollections.find(query).toArray();
+            res.send(result)
+        })
+
+    } finally {
+
+    }
+}
+run().catch(e => console.error(e))
 
 
 app.get('/', (req, res) => {
